5863e292cc0f7b0586c0188656bd4078d36a5204
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-28 22:57:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
+ if block_given?
+ i=0 
+ even_array = []
+ while i.even? 
+ yield(collection[i])
+   even_array.push (collection[i])
+ end
+ i += 1 
+ end
+ even_array
+ else
+   "This block should not run!"
+ end
  # your code here!
 end
