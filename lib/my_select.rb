52b8ae31a59d612083f2a894174591145cf026ec
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-28 23:01:32 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,16 +1,4 @@
 def my_select(collection)
- if block_given?
- i=0 
- even_array = []
- while i.even? 
- yield(collection[i])
-   even_array.push (collection[i])
- end
- i += 1 
- end
- even_array
- else
-   "This block should not run!"
- end
+ collection.select {|num|num.even?}
  # your code here!
 end
